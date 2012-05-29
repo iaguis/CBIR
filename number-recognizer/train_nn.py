069faa41bdd61785eb0f06debbb9a184a6f20e67
Iago López <iaguis@gmail.com>
2012-05-29 12:59:57 +0200

    Forgot to commit some files :P

--- number-recognizer/train_nn.py
+++ number-recognizer/train_nn.py
@@ -49,13 +49,13 @@ def nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels,
     for i in range(m):
         y_encoded = np.zeros((num_labels,1))
         # FIXME Change this when training the net
-        what = y[i]
-        if what == 0:
-            what = 9
-        else:
-            what = y[i]-1
+        #what = y[i]
+        #if what == 0:
+            #what = 9
+        #else:
+            #what = y[i]-1
 
-        y_encoded[what] = [1]
+        y_encoded[y[i]] = [1]
         
         
         a_1 = X[i, :].T.copy()
@@ -94,47 +94,72 @@ def nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels,
     grad = np.concatenate((Theta1_grad.flatten(), Theta2_grad.flatten()))
     
     return J, grad
+    
+def predict(Theta1, Theta2, X):
+    m = X.shape[0]
+    num_labels = Theta2.shape[0]
         
+    h1 = sigmoid(np.dot(np.concatenate((np.ones((m, 1)), X), 1), Theta1.T))
+    h2 = sigmoid(np.dot(np.concatenate((np.ones((m, 1)), h1), 1), Theta2.T))
+    #p = np.max(h2, axis=1)[np.newaxis].T
+    p = h2.argmax(axis=1)[np.newaxis].T
+    
+    return p
+    
+def train(X, y):
+    input_layer_size  = 400
+    hidden_layer_size = 25
+    num_labels = 10
+    
+    initial_Theta1 = rand_initialize_weights(input_layer_size, hidden_layer_size)
+    initial_Theta2 = rand_initialize_weights(hidden_layer_size, num_labels)
+       
+    initial_nn_params = np.concatenate((initial_Theta1.flatten(), initial_Theta2.flatten()))
         
+    lambd = 3
+       
+    def costFunction(p):
+        return nn_cost_function(p, input_layer_size, hidden_layer_size, num_labels, X, y, lambd)
+        
+       
+    nn_params, cost, i = fmincg(costFunction, initial_nn_params, 50)
+        
+    theta_size = hidden_layer_size*(input_layer_size+1)
+        
+    Theta1 = nn_params[:theta_size].reshape(hidden_layer_size, input_layer_size+1).copy()
+    Theta2 = nn_params[theta_size:].reshape(num_labels, hidden_layer_size+1).copy()
+    if cost[len(cost)-1] < 0.8:
+        np.savetxt("Theta1.txt", Theta1)
+        np.savetxt("Theta2.txt", Theta2)
+    return Theta1, Theta2
+        
+def usage():
+    print("Usage: ", sys.argv[0], " train|predict")
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     try:
-        input_layer_size  = 400
-        hidden_layer_size = 25
-        num_labels = 10
+        if len(sys.argv) < 2:
+            usage()
+            return
         
-        X, y = read_training_examples("training_X.txt", "training_y.txt")
-        #Theta1 = np.genfromtxt("Theta1.txt")
-        #Theta2 = np.genfromtxt("Theta2.txt")
+        if sys.argv[1] == "train":
+            X, y = read_training_examples("training_X.txt", "training_y.txt")
+            Theta1, Theta2 = train(X, y)
+        elif sys.argv[1] == "predict":
+            X, y = read_training_examples("training_X.txt", "training_y.txt")
+            Theta1 = np.genfromtxt("Theta1.txt")
+            Theta2 = np.genfromtxt("Theta2.txt")
+        else:
+            usage()
+            return
         
-        initial_Theta1 = rand_initialize_weights(input_layer_size, hidden_layer_size)
-        initial_Theta2 = rand_initialize_weights(hidden_layer_size, num_labels)
-        
-        initial_nn_params = np.concatenate((initial_Theta1.flatten(), initial_Theta2.flatten()))
-        
-        lambd = 3
-        
-        def costFunction(p):
-            return nn_cost_function(p, input_layer_size, hidden_layer_size, num_labels, X, y, lambd)
-        
-        #sp.fmin_cg(costFunction, initial_nn_params)
-            
-        #J, grad = costFunction(initial_nn_params)
-        
-        nn_params, cost, i = fmincg(costFunction, initial_nn_params, 50)
-        print(nn_params)
-        print(cost)
-        #nn_params = np.concatenate((Theta1.flatten(), Theta2.flatten()))
-        
-        #J, grad = nn_cost_function(nn_params, 400, 25, 10, X, y, 1) # debug
-        #print(J)
-        #print(grad)
-        
-        
-    except err:
-        print >>sys.stderr, err.msg
+        pred = predict(Theta1, Theta2, X)
+
+        print("Training Set Accuracy ", np.mean(pred == y[np.newaxis].T))
+    except Exception:
+        print(Exception.msg, file=sys.stderr)
         return -1
 
 if __name__ == "__main__":
